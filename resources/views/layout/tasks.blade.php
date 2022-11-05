3b25aacbf741c8d7edcfb8780f8f3330a0e1e218
burhanmt <burhanmt@icloud.com>
2022-11-05 08:48:56 +0000

    the application scaffolding was added

--- resources/views/layout/tasks.blade.php
+++ resources/views/layout/tasks.blade.php
@@ -5,11 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MLP To-Do</title>
 
+    <!-- JS & Bootstrap Css -->
     @vite(['resources/js/app.js'])
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
-
+<div class="container-sm mt-4 containerSize">
+    <div class="row">
+        <div class="col-12">
+            <!-- Product Logo -->
+            <div class="productLogo"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <!-- Navigation Menu -->
+            @include('partials.nav')
+        </div>
+        <div class="col-md-8">
+            <!-- Page content -->
+            @yield('content')
+        </div>
+    </div>
+</div>
 </body>
 </html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.tasks')
+@section('content')
+    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd
+    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd
+    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd
+    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd    This is a test. dsfd
+@endsection
